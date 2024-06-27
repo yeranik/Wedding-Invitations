bdfc143611a2e8577bb7ea59d31311b687d83fcf
yeranik <136292592+yeranik@users.noreply.github.com>
2024-06-26 19:41:45 -0600

    added ticket numbers

--- src/greeting.js
+++ src/greeting.js
@@ -11,9 +11,8 @@ console.log("name2", name2);
 
 if (name1 && name2) {
   // Update the webpage with personalized content for two people
-  document.getElementById(
-    "greeting"
-  ).textContent = `Hello, ${name1}! and ${name2}! Welcome to our website`;
+  document.getElementById("greeting").textContent = `${name1}! & ${name2}! `;
+  document.getElementById("tickets").textContent = "(2)PERSONAS";
 } else if (name1) {
   // Update the webpage with personalized content for the first person only
   document.getElementById(
