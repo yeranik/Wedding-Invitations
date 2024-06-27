cf86883d7db8348749465ec2179f94011ed7bc4a
yeranik <136292592+yeranik@users.noreply.github.com>
2024-06-26 19:26:34 -0600

    test 3

--- src/greeting.js
+++ src/greeting.js
@@ -3,23 +3,28 @@ const queryString = window.location.search;
 console.log("queryString", queryString);
 const urlParams = new URLSearchParams(queryString);
 console.log("urlParams", urlParams);
-const name1 = urlParams.get('name1');
-const name2 = urlParams.get('name2');
+const name1 = urlParams.get("name1");
+const name2 = urlParams.get("name2");
 
 console.log("name1", name1);
 console.log("name2", name2);
 
-
 if (name1 && name2) {
   // Update the webpage with personalized content for two people
-  document.getElementById('greeting').textContent = Hello, ${name1}! and ${name2}! Welcome to our website.;
+  document.getElementById(
+    "greeting"
+  ).textContent = `Hello, ${name1}! and ${name2}! Welcome to our website`;
 } else if (name1) {
   // Update the webpage with personalized content for the first person only
-  document.getElementById('greeting').textContent = Hello, ${name1}! Welcome to our website.;
+  document.getElementById(
+    "greeting"
+  ).textContent = `Hello, ${name1}! Welcome to our website`;
 } else if (name2) {
   // Update the webpage with personalized content for the second person only
-  document.getElementById('greeting').textContent = Hello, ${name2}! Welcome to our website.;
+  document.getElementById(
+    "greeting"
+  ).textContent = `Hello, ${name2}! Welcome to our website.`;
 } else {
   // Update the webpage with a generic greeting
-  document.getElementById('greeting').textContent = 'Welcome to our website.';
+  document.getElementById("greeting").textContent = "Welcome to our website.";
 }
