aef96a39c20acdbde27f97138a92806495c5571e
yeranik <136292592+yeranik@users.noreply.github.com>
2024-06-26 19:20:18 -0600

    test 2

--- src/greeting.js
+++ src/greeting.js
@@ -1,14 +1,17 @@
 const queryString = window.location.search;
+console.log("queryString", queryString);
 const urlParams = new URLSearchParams(queryString);
+console.log("urlParams", urlParams);
 const name1 = urlParams.get('name1');
 const name2 = urlParams.get('name2');
 
-console.log("name1", name1)
-console.log("name2", name2)
+console.log("name1", name1);
+console.log("name2", name2);
+
 
 if (name1 && name2) {
   // Update the webpage with personalized content for two people
-  document.getElementById('greeting').textContent = Hello, ${name1} and ${name2}! Welcome to our website.;
+  document.getElementById('greeting').textContent = Hello, ${name1}! and ${name2}! Welcome to our website.;
 } else if (name1) {
   // Update the webpage with personalized content for the first person only
   document.getElementById('greeting').textContent = Hello, ${name1}! Welcome to our website.;
