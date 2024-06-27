71c5fa3061ae18c887abccec9624bdfbc8d12723
yeranik <136292592+yeranik@users.noreply.github.com>
2024-06-26 19:49:19 -0600

    testing with tickets

--- src/greeting.js
+++ src/greeting.js
@@ -8,22 +8,14 @@ const name2 = urlParams.get("name2");
 
 console.log("doc", document.getElementById("greeting"));
 console.log("name2", name2);
+const tickets = name1 && name2 ? 2 : 1;
 
 if (name1 && name2) {
   // Update the webpage with personalized content for two people
-  document.getElementById("greeting").textContent = `${name1}! & ${name2}! `;
-  document.getElementById("tickets").textContent = "(2)PERSONAS";
-} else if (name1) {
-  // Update the webpage with personalized content for the first person only
-  document.getElementById(
-    "greeting"
-  ).textContent = `Hello, ${name1}! Welcome to our website`;
-} else if (name2) {
-  // Update the webpage with personalized content for the second person only
-  document.getElementById(
-    "greeting"
-  ).textContent = `Hello, ${name2}! Welcome to our website.`;
+  document.getElementById("greeting").textContent = `${name1} & ${name2} `;
+  document.getElementById("tickets").textContent = `(${tickets})PERSONAS`;
 } else {
   // Update the webpage with a generic greeting
-  document.getElementById("greeting").textContent = "Welcome to our website.";
+  document.getElementById("greeting").textContent = `${name1} `;
+  document.getElementById("tickets").textContent = `(${tickets})PERSONA`;
 }
